98e96bdfe72c43f343305d80e57b936d3d737bbb
Connor Ruggles <conruggles@gmail.com>
2020-03-20 00:41:48 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git pull
+
+npm ci
+
+status=$?
+if [ $status -ne 0 ]; then
+    echo "something went wrong running npm ci"
+    exit 1
+fi
+
+npm run ci
+
+status=$?
+if [ $status -ne 0 ]; then
+    echo "something went wrong running npm run build"
+    exit 1
+fi
+
+/bin/systemctl restart budget_api.service
